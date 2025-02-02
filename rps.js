034c83b9265a4e030af6b9af09727230c670b6d4
Jorge Serna <jorge.sernap90@gmail.com>
2025-02-02 00:00:38 -0600

    Add playRound function with if conditions

--- rps.js
+++ rps.js
@@ -20,3 +20,60 @@ function getHumanChoice() {
 }
 
 console.log(getHumanChoice());
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock") 
+    {
+        if (computerChoice === "scissors") 
+        {
+            console.log("You win! Rock beats Scissors.");
+            humanScore++;
+        }
+        else if (computerChoice === "paper")
+        {
+            console.log("You lose! Paper beats Rock.");
+            computerScore++;
+        }
+        else
+        {
+            console.log("It's a tie.")
+        }
+    }
+    else if (humanChoice === "paper")
+    {
+        if (computerChoice === "rock")
+        {
+            console.log("You win! Paper beats Rock.");
+            humanScore++;
+        }
+        else if (computerChoice === "scissors")
+        {
+            console.log("You lose! Scissors beat Paper.");
+            computerScore++;
+        }
+        else
+        {
+            console.log("It's a tie.")
+        }
+    }
+    else
+    {
+        if (computerChoice === "paper")
+        {
+            console.log("You win! Scissors beat Paper.")
+            humanScore++;
+        }
+        else if (computerChoice === "rock")
+        {
+            console.log("You lose! Rock beats Scissors.")
+            computerScore++;
+        }
+        else
+        {
+            console.log("It's a tie.")
+        }
+    }
+}
